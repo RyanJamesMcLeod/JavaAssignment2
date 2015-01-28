11d46785ed6d3d407e0534ef085f56fc3836e4f3
Ryan <Ryan@10.55.2.95>
2015-01-28 10:50:39 -0500

    Added Behavior 2: No Id or name throw exception

--- test/cpd4414/assign2/OrderQueueTest.java
+++ test/cpd4414/assign2/OrderQueueTest.java
@@ -53,7 +53,7 @@ public class OrderQueueTest {
     }
 
     @Test
-    public void testWhenCustomerExistsAndPurchasesExistThenTimeReceivedIsNow() {
+    public void testWhenCustomerExistsAndPurchasesExistThenTimeReceivedIsNow() throws Exception {
         OrderQueue orderQueue = new OrderQueue();
         Order order = new Order("CUST00001", "ABC Construction");
         order.addPurchase(new Purchase("PROD0004", 450));
@@ -63,6 +63,23 @@ public class OrderQueueTest {
         long expResult = new Date().getTime();
         long result = order.getTimeReceived().getTime();
         assertTrue(Math.abs(result - expResult) < 1000);
+    }
+    
+    @Test
+    public void testWhenCustomerNameAndIdDoNotExistThenThrowException() {
+        boolean didthrow = false;
+        OrderQueue orderQueue = new OrderQueue();
+        Order order = new Order("", "");
+        order.addPurchase(new Purchase("PROD0004", 450));
+        order.addPurchase(new Purchase("PROD0006", 250));
+        try {
+            orderQueue.add(order);
+        }
+        catch (Exception ex){
+            didthrow = true;
+        }
+        
+        assertTrue(didthrow);
     }
     
 }
